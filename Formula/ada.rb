90b511ae21187b7c2eecb3258ee8d17cc3ef5b46
Sai Krishna Bolla <85215739+saikrishnabolla@users.noreply.github.com>
2026-03-26 22:19:13 -0400

    Update Ada to v0.1.2

--- Formula/ada.rb
+++ Formula/ada.rb
@@ -7,7 +7,7 @@ class Ada < Formula
   on_macos do
     on_arm do
       url "https://github.com/saikrishnabolla/homebrew-tap/releases/download/v0.1.2/ada-0.1.2-darwin-arm64.tar.gz"
-      sha256 "aab6844a0ffb4b138fc7d82a5c3b7c2b8608f80a48a83f3081258544a339a45e"
+      sha256 "fbc6ba4f8f61a607f8e08dabde7f66d3282d882eda75f49555f14583329d9695"
     end
     on_intel do
       url "https://github.com/saikrishnabolla/homebrew-tap/releases/download/v0.1.2/ada-0.1.2-darwin-x64.tar.gz"
